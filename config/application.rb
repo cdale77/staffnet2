4f58df686957948bfaafe4d68be4b5e59d90ce73
cdale77 <cdale77@gmail.com>
2013-08-14 14:28:31 -0700

    devise + layout

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module Staffnet2
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
     config.generators do |g|
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Staffnet2::Application.routes.draw do
+  get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +54,6 @@ Staffnet2::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root  'admin/static_pages#home'
 end
