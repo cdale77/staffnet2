0d70cce50f1ee22a2d952c22449fb8cfca0bebac
cdale77 <cdale77@gmail.com>
2013-08-14 11:47:19 -0700

    secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,16 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Staffnet2::Application.config.secret_key_base = 'a58ac12377d3475e7309c6c8124d93f467d3c0ebdb6eaed953bc6bc007bac40146cf210275c4d7d4897c376ff953b3ba591fbe37853c5eb406f998f01079af28'
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+Staffnet2::Application.config.secret_key_base = secure_token
