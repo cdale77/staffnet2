7ecc940455593523624a026bad94b7ce95b45f5a
Brad Johnson <me@example.com>
2013-11-16 11:21:54 -0800

    comments regarding user authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  # using before_filters for authorization here because it is awkward in Pundit. Tests for this code are in
+  # the user features spec.
   before_filter :super_admin, only: [:new, :create, :edit, :update]
   before_filter :admin, only: [:index, :destroy]
   before_filter :authorize_user, only: :show
