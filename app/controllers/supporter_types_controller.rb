73d4e49d1aed3bd55e764492758418564f4d0895
Brad Johnson <cdale77@gmail.com>
2013-12-07 05:51:01 -0800

    supporter type auth

--- app/controllers/supporter_types_controller.rb
+++ app/controllers/supporter_types_controller.rb
@@ -1,13 +1,16 @@
 class SupporterTypesController < ApplicationController
 
-  before_filter :admin
+  include Pundit
+  after_filter :verify_authorized
 
   def new
     @supporter_type = SupporterType.new
+    authorize @supporter_type
   end
 
   def create
     @supporter_type = SupporterType.new(supporter_type_params)
+    authorize @supporter_type
     if @supporter_type.save
       flash[:success] = 'Success.'
       redirect_to supporter_types_path
@@ -18,14 +21,17 @@ class SupporterTypesController < ApplicationController
 
   def index
     @supporter_types = SupporterType.all
+    authorize @supporter_types
   end
 
   def edit
     @supporter_type = SupporterType.find(params[:id])
+    authorize @supporter_type
   end
 
   def update
     @supporter_type = SupporterType.find(params[:id])
+    authorize @supporter_type
     if @supporter_type.update_attributes(supporter_type_params)
       flash[:success] = 'Success.'
       redirect_to supporter_types_path
