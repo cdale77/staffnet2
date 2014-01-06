d9ff2909345ec1e986ac2886b41cade8e4762dbe
Brad Johnson <cdale77@gmail.com>
2014-01-06 05:12:10 -0800

    rspec green

--- Gemfile
+++ Gemfile
@@ -30,7 +30,7 @@ group :development, :test do
   gem 'rspec-rails', '2.14.0'
   #gem 'shoulda-matchers', github: "thoughtbot/shoulda-matchers" , branch: 'dp-rails-four'
   gem 'capybara', '2.1.0'
-  gem 'selenium-webdriver', '2.0.0'
+  gem 'selenium-webdriver', '2.39.0'
   gem 'factory_girl_rails', '4.2.1'
   gem 'debugger'
 end
--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module Staffnet2
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.i18n.enforce_available_locales = true
+
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.filter_parameters += [:password, :password_confirmation]
     config.autoload_paths += %W(#{config.root}/lib/validations)
