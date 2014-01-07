47f9150726e3cb810ee4e9a2fda13a62055ecc10
Brad Johnson <cdale77@gmail.com>
2014-01-07 13:32:50 -0800

    fixed supporter index links

--- spec/features/supporter_pages_spec.rb
+++ spec/features/supporter_pages_spec.rb
@@ -7,6 +7,7 @@ describe 'SupporterPages' do
   subject { page }
 
   let(:super_admin) { FactoryGirl.create(:super_admin) }
+  let(:manager) { FactoryGirl.create(:manager) }
   let!(:supporter_type) { FactoryGirl.create(:supporter_type) } # so there's a supporter type to pick for #new
   let(:supporter) { FactoryGirl.create(:supporter) }
 
@@ -138,6 +139,52 @@ describe 'SupporterPages' do
       before { visit supporter_path(supporter) }
       it 'should destroy a supporter' do
         expect { click_link 'delete' }.to change(Supporter, :count).by(-1)
+      end
+    end
+  end
+
+  #### AS MANAGER USER ####
+
+  # manager users should not see edit or delete links
+  describe 'as manager user' do
+    before do
+      visit new_user_session_path
+      fill_in 'Email',    with: manager.email
+      fill_in 'Password', with: manager.password
+      click_button 'Sign in'
+    end
+
+    after do
+      logout(:manager)
+    end
+
+    describe 'show' do
+      describe 'page' do
+        before { visit supporter_path(supporter) }
+        describe 'page' do
+          describe 'links' do
+            it { should_not have_link('edit', href: edit_supporter_path(supporter)) }
+            it { should_not have_link('delete', href: supporter_path(supporter)) }
+          end
+        end
+      end
+    end
+
+    describe 'index' do
+      before do
+        5.times { FactoryGirl.create(:supporter) }
+        visit supporters_path
+      end
+
+      after { Supporter.delete_all }
+
+      describe 'page' do
+        it 'should have the right links' do
+          Supporter.all.each do |supporter|
+            expect(page).to have_link('details', supporter_path(supporter))
+            expect(page).to_not have_link('edit', edit_supporter_path(supporter))
+          end
+        end
       end
     end
   end
