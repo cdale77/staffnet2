dd17b9586d42998bb3464994cb5ad43bc4f5f7bb
Brad Johnson <cdale77@gmail.com>
2014-01-21 11:40:09 -0800

    basic cim profile creation code

--- lib/cim/cim.rb
+++ lib/cim/cim.rb
@@ -1,11 +1,40 @@
 module Cim	
 
 
-	class Connection
+  def self.connection
+    ActiveMerchant::Billing::AuthorizeNetCimGateway.new(  login: ENV['CIM_LOGIN'],
+                                                            password: ENV['CIM_PASSWORD'],
+                                                            test: true )
+  end
 
-		def initialize
-      ActiveMerchant::Billing::AuthorizeNetCimGateway.new(  login: ENV['CIM_LOGIN'],
-                                                            password: ENV['CIM_PASSWORD'])
-		end
-	end
+  class Profile 
+
+    attr_reader :cim_id
+    attr_reader :params
+
+    def initialize(supporter_id, supporter_email = '')
+      @supporter_id = supporter_id.to_s
+      @supporter_email = supporter_email
+      @cim_id = ''
+      @params = {}
+    end
+
+    def store
+      result = Cim.connection.create_customer_profile(profile: customer_profile)
+      @params = result.params
+      @cim_id = result.params['customer_profile_id']
+      raise Exceptions::CimProfileError unless @cim_id
+    end
+
+
+    private
+      def customer_profile
+        { 
+          merchant_customer_id: @supporter_id,
+          email: @supporter_email
+        }
+      end
+  end
+
+
 end
---
+++ lib/cim/exceptions.rb
@@ -0,0 +1,3 @@
+module Exceptions
+  class CimProfileError < StandardError; end
+end
