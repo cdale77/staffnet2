b2d39b7a78e5756758de3bcdbf480b3eba5a2d09
Brad Johnson <cdale77@gmail.com>
2014-03-04 10:42:56 -0800

    add sendy lib stub

---
+++ lib/sendy/CreateSendyUpdateRecord.rb
@@ -0,0 +1,18 @@
+class CreateSendyUpdateRecord
+
+  ## This class wraps a few methods that add the appropriate record to the SendyList table for later updating.
+
+  def self.subscribe
+
+  end
+
+  def self.unsubscribe
+
+  end
+
+
+  private
+
+
+
+end
