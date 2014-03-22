84941ccec40ebca24dfb19046c017046f73279ab
Brad Johnson <cdale77@gmail.com>
2014-03-21 17:12:27 -0700

    cleanup

--- lib/sendy/sendy.rb
+++ lib/sendy/sendy.rb
@@ -11,11 +11,9 @@ module Sendy
 
       client = Sendyr::Client.new(sendy_list.sendy_list_identifier)
 
-      supporters = sendy_list.supporters.where("email_1 <> '' AND do_not_email IS FALSE")
 
-      supporters.find_each do |supporter|
 
-        puts "Updating #{supporters.count.to_s} supporters. . . "
+      sendy_list.supporters.where("email_1 <> '' AND do_not_email IS FALSE").find_each do |supporter|
 
         begin
           sendy_status = client.subscription_status(email: supporter.email_1).to_s
@@ -24,12 +22,8 @@ module Sendy
           next
         end
 
-        puts "Status for supporter id #{supporter.id.to_s}, email: #{supporter.email_1} is: #{sendy_status}"
-
         #if the status recieved from Sendy matches the existing status, no updates
-        if sendy_status == supporter.sendy_status
-          puts "Sendy status match. No update"
-        else
+        if sendy_status != supporter.sendy_status
 
           if Staffnet2::SendyUpdate.create( supporter_id: supporter.id,
                                             sendy_list_id: sendy_list.id,
@@ -37,9 +31,9 @@ module Sendy
                                             new_sendy_email: supporter.email_1,
                                             action: 'update_database',
                                             new_sendy_status: sendy_status)
-            puts "Created update record"
+            puts "Created update record for supporter id #{supporter.id.to_s}"
           else
-            puts "Something went wrong creating update record"
+            puts "Something went wrong creating update record for supporter id #{supporter.id.to_s}"
           end
         end
       end
@@ -60,7 +54,7 @@ module Sendy
         end
 
         case update.action
-
+          
           when 'update_database'
             unless supporter.sendy_status == update.new_sendy_status
               supporter.sendy_status = update.new_sendy_status
@@ -75,6 +69,7 @@ module Sendy
               supporter.sendy_status = 'subscribed'
               mark_as_completed(update) if supporter.save
             end
+
         end
       end
     end
