d3f1a42b26de6f70388d60ae7448575d88be24b1
Brad Johnson <cdale77@gmail.com>
2014-03-23 11:11:42 -0700

    back

--- app/controllers/supporters_controller.rb
+++ app/controllers/supporters_controller.rb
@@ -31,8 +31,7 @@ class SupportersController < ApplicationController
 
   def index
     @search = Supporter.search(params[:q])
-    #@supporters = @search.result.paginate(:page => params[:page], per_page: 50)
-    @supporters = Supporter.all.paginate(:page => params[:page], per_page: 50)
+    @supporters = @search.result.paginate(:page => params[:page], per_page: 50)
     @search.build_condition
     authorize @supporters
   end
