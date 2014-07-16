ee3325803a4d11bcc573da38df78981dbb27df7c
Brad Johnson <cdale77@gmail.com>
2014-07-16 14:47:06 -0700

    wip

--- app/controllers/sendy_lists_controller.rb
+++ app/controllers/sendy_lists_controller.rb
@@ -5,10 +5,12 @@ class SendyListsController < ApplicationController
 
   def new
     @sendy_list = SendyList.new
+    authorize @sendy_list
   end
 
   def create
     @sendy_list = SendyList.new(sendy_list_params)
+    authorize @sendy_list
     if @sendy_list.save
       flash[:success] = 'Saved new Sendy list.'
       redirect_to sendy_lists_path
@@ -20,14 +22,17 @@ class SendyListsController < ApplicationController
 
   def index
     @sendy_lists = SendyList.all
+    authorize @sendy_lists
   end
 
   def edit
     @sendy_list = SendyList.find(params[:id])
+    authorize @sendy_list
   end
 
   def update
     @sendy_list = SendyList.find(params[:id])
+    authorize @sendy_list
     if @sendy_list.update_attributes(sendy_list_params)
       flash[:success] = 'Sendy list updated.'
       redirect_to sendy_lists_path
@@ -36,7 +41,8 @@ class SendyListsController < ApplicationController
     end
   end
 
-  def sendy_list_params
-    params.require(:sendy_list).permit(:supporter_type_id, :name, :sendy_list_identifier)
-  end
+  private
+    def sendy_list_params
+      params.require(:sendy_list).permit(:supporter_type_id, :name, :sendy_list_identifier)
+    end
 end
