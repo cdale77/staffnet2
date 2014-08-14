f3ee60c2edd4598df05ef428fcc12af626ac6195
Brad Johnson <cdale77@gmail.com>
2014-08-14 12:52:01 -0700

    hard code multipliers for now

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -88,11 +88,13 @@ class Shift < ActiveRecord::Base
   end
 
   def reported_monthly_value
-    reported_monthly_cc_amt * self.shift_type.monthly_cc_multiplier
+    #reported_monthly_cc_amt * self.shift_type.monthly_cc_multiplier
+    reported_monthly_cc_amt * 7
   end
 
   def reported_quarterly_value
-    reported_quarterly_cc_amt * self.shift_type.quarterly_cc_multiplier
+    #reported_quarterly_cc_amt * self.shift_type.quarterly_cc_multiplier
+    reported_quarterly_cc_amt *3
   end
 
 
