3e2be4e4af0801343d398c0abf68f56a36b89ebf
Brad Johnson <cdale77@gmail.com>
2014-08-19 13:51:58 -0700

    remove migrations

--- db/migrate/20131207003638_create_supporter_types.rb
+++
@@ -1,9 +0,0 @@
-class CreateSupporterTypes < ActiveRecord::Migration
-  def change
-    create_table :supporter_types do |t|
-      t.string  :name, :default => ''
-
-      t.timestamps
-    end
-  end
-end
