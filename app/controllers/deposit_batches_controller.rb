2c5ea39756b1a8c980dfe744928efa9db836e0e3
Brad Johnson <cdale77@gmail.com>
2014-08-21 14:13:21 -0700

    deposit batch index method

--- app/controllers/deposit_batches_controller.rb
+++ app/controllers/deposit_batches_controller.rb
@@ -11,7 +11,7 @@ class DepositBatchesController < ApplicationController
 
   def index
     DepositBatch.batch_up
-    @deposit_batches = DepositBatch.where(approved: false)
+    @deposit_batches = DepositBatch.first(20)
     authorize @deposit_batches
   end
 
