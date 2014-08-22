f6dad243090fea72e91f204ddb79a26eca675bf9
Brad Johnson <cdale77@gmail.com>
2014-08-21 17:33:27 -0700

    wip

--- app/controllers/installments_controller.rb
+++
@@ -1,9 +0,0 @@
-class InstallmentsController < ApplicationController
-
-  # using before_filters for authorization here because it is awkward in Pundit.
-  before_filter :admin, only: [:review]
-
-  def review
-    @deposit_batches = DepositBatch.installment_batches_to_be_approved
-  end
-end
