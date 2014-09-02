40a1299dbc7b62daa5c70fb551196f40c64ca763
Brad Johnson <cdale77@gmail.com>
2014-09-01 20:25:24 -0700

    refactor wip

--- lib/exports/exports_donation_history.rb
+++ lib/exports/exports_donation_history.rb
@@ -53,8 +53,56 @@ module Exports
       return column_names
     end
 
+    def self.supporter_fields(supporter, donations)
+      donations_count = donations.count.to_s
+      donations_amount = donations.map(&:total_value).inject(0, &:+)
+
+      [  supporter.id.to_s,
+         supporter.full_name,
+         supporter.address1,
+         supporter.address2,
+         supporter.address_city,
+         supporter.address_state,
+         supporter.address_zip,
+         supporter.email_1,
+         supporter.email_2,
+         supporter.phone_mobile,
+         supporter.phone_mobile_bad,
+         supporter.phone_home,
+         supporter.phone_home_bad,
+         supporter.phone_alt,
+         supporter.phone_alt_bad,
+         supporter.supporter_type.name,
+         supporter.prospect_group,
+         supporter.notes,
+         supporter.do_not_email,
+         supporter.do_not_call,
+         supporter.do_not_mail,
+         donations_count,
+         donations_amount,
+         supporter.is_sustainer? ]
+    end
+
+    def self.donation_fields(donation)
+      # look up the most recent payment
+      payment = donation.payments.first
+      profile = payment ? payment.payment_profile : PaymentProfile.new
+      cc_info = profile ? profile.short_version : ""
+
+      [  donation.source,
+         donation.date,
+         donation.amount,
+         donation.donation_type,
+         donation.frequency,
+         donation.sub_month,
+         !donation.captured,
+         donation.cancelled,
+         cc_info,
+         donation.notes ]
+    end
+
     def self.perform
-# generate the CSV file
+
       puts "Generating the CSV file. This might take a while."
       csv_file = CSV.generate do |csv_row|
 
@@ -65,52 +113,11 @@ module Exports
 
           donations = supporter.donations.limit(5)
 
-          donations_count = donations.count.to_s
-          donations_amount = donations.map(&:total_value).inject(0, &:+)
-
-          #set the initial supporter fields array
-          supporter_fields = [  supporter.id.to_s,
-                                supporter.full_name,
-                                supporter.address1,
-                                supporter.address2,
-                                supporter.address_city,
-                                supporter.address_state,
-                                supporter.address_zip,
-                                supporter.email_1,
-                                supporter.email_2,
-                                supporter.phone_mobile,
-                                supporter.phone_mobile_bad,
-                                supporter.phone_home,
-                                supporter.phone_home_bad,
-                                supporter.phone_alt,
-                                supporter.phone_alt_bad,
-                                supporter.supporter_type.name,
-                                supporter.prospect_group,
-                                supporter.notes,
-                                supporter.do_not_email,
-                                supporter.do_not_call,
-                                supporter.do_not_mail,
-                                donations_count,
-                                donations_amount,
-                                supporter.is_sustainer? ]
+          supporter_fields = self.supporter_fields(supporter, donations)
 
           donations.each do |donation|
 
-            # look up the most recent payment
-            payment = donation.payments.first
-            profile = payment ? payment.payment_profile : PaymentProfile.new
-            cc_info = profile ? profile.short_version : ""
-
-            donation_fields =  [  donation.source,
-                                  donation.date,
-                                  donation.amount,
-                                  donation.donation_type,
-                                  donation.frequency,
-                                  donation.sub_month,
-                                  !donation.captured,
-                                  donation.cancelled,
-                                  cc_info,
-                                  donation.notes ]
+            donation_fields =  self.donation_fields(donation)
 
             # add the donations fields to the supporter fields array
             donation_fields.each { |field| supporter_fields << field }
