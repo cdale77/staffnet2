7b2ce92f5250389590ae4253cf5b23438bf36601
Brad Johnson <cdale77@gmail.com>
2014-09-05 21:38:35 -0700

    minor improvements

--- app/jobs/employee_fundraising_calculations_job.rb
+++ app/jobs/employee_fundraising_calculations_job.rb
@@ -5,33 +5,42 @@ class EmployeeFundraisingCalculationsJob < ActiveJob::Base
 
   def perform
     Employee.active.each do |employee|
+      results = calculate_values(employee)
+      employee.assign_attributes(results)
+      employee.save
+    end
+
+    def calculate_values(employee)
       shifts = employee.shifts
       shifts_this_week = shifts.where(date: (Date.today.beginning_of_week..Date.today))
       fundraising_shifts = shifts.select { |s| s.fundraising_shift }
       fundraising_shifts_this_week = shifts_this_week.select { |s| s.fundraising_shift }
       donations = employee.donations
-      donations_this_week = donations.select { |d| (Date.today.beginning_of_week..Date.today).include?(d.shift.date) }
       successful_donations = donations.select { |d| d.captured }
-      successful_donations_this_week =  donations.select { |d| (Date.today.beginning_of_week..Date.today).include?(d.shift.date) }
-      raised_lifetime = successful_donations.sum(&:total_value)
-      raised_this_week = successful_donations_this_week.sum(&:total_value)
-      average_lifetime = raised_lifetime / fundraising_shifts.count
-      average_this_week = raised_this_week / fundraising_shifts_this_week.count
+      donations_this_week = donations.select { |d| (Date.today.beginning_of_week..Date.today).include?(d.shift.date) }
+      successful_donations_this_week = successful_donations.select { |d| (Date.today.beginning_of_week..Date.today).include?(d.shift.date) }
+      {
+          shifts: shifts.count,
+          shifts_this_week: shifts_this_week.count,
+          fundraising_shifts: fundraising_shifts.count,
+          fundraising_shifts_this_week: fundraising_shifts_this_week.count,
+          donations: donations.count,
+          donations_this_week: donations_this_week.count,
+          successful_donations: successful_donations.count,
+          successful_donations_this_week:  successful_donations_this_week.count,
+          raised_lifetime: successful_donations.sum(&:total_value),
+          raised_this_week: successful_donations_this_week.sum(&:total_value),
+          average_lifetime: calculate_average(raised_lifetime, fundraising_shifts.count),
+          average_this_week: calculate_averge(raised_this_week / fundraising_shifts_this_week.count)
+      }
+    end
 
-      employee.shifts_lifetime = shifts.count
-      employee.shifts_this_week = shifts_this_week.count
-      employee.fundraising_shifts_lifetime = fundraising_shifts.count
-      employee.fundraising_shifts_this_week = fundraising_shifts_this_week.count
-      employee.donations_lifetime = donations.count
-      employee.donations_this_week = donations_this_week.count
-      employee.successful_donations_lifetime = successful_donations.count
-      employee.successful_donations_this_week = successful_donations_this_week.count
-      employee.raised_lifetime = raised_lifetime
-      employee.raised_this_week = raised_this_week
-      employee.average_lifetime = average_lifetime
-      employee.average_this_week = average_this_week
-
-      employee.save
+    def calculate_average(raised, shifts_count)
+      if fundraising_shifts_count > 0
+        raised / shifts_count
+      else
+        0
+      end
     end
   end
 end
