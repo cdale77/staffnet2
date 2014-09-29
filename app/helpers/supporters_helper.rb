1c28b1d8aa9e21d97471099154b469c7003e2764
Brad Johnson <cdale77@gmail.com>
2014-09-29 14:54:43 -0700

    supporters caching

--- app/helpers/supporters_helper.rb
+++ app/helpers/supporters_helper.rb
@@ -1,11 +1,15 @@
 module SupportersHelper 
 
+  def cache_key_for_supporters
+    count          = Supporter.count
+    max_updated_at = Supporter.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "supporters/all-#{count}-#{max_updated_at}"
+  end
+
 
   def cache_key_for_supporter_types
     count          = SupporterType.count
     max_updated_at = SupporterType.maximum(:updated_at).try(:utc).try(:to_s, :number)
     "supporter_types/all-#{count}-#{max_updated_at}"
   end
-
 end
-
