38e3e1c460a12e67fb1705913e49197544390efd
Brad Johnson <cdale77@gmail.com>
2014-09-30 10:11:28 -0700

    basic model

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,7 +42,7 @@ RSpec.configure do |config|
               :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'text/xml', 'User-Agent'=>'Ruby'}).
          to_return(:status => 200, :body => "", :headers => {})
   end
-  =end
+=end
 
 
 
