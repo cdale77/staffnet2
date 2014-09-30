e959589ee9d84f4e630b0fd1f363f19ae719b9ae
Brad Johnson <cdale77@gmail.com>
2014-09-30 09:16:51 -0700

    no more stubbing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "spec_helper"
-WebMock.disable_net_connect!(allow_localhost: true)
+#WebMock.disable_net_connect!(allow_localhost: true)
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
