550b6ea61641366c4f6273c06e163c6c28939980
Brad Johnson <cdale77@gmail.com>
2014-10-01 10:20:37 -0700

    dupe record service working

--- app/services/create_duplicate_records_service.rb
+++ app/services/create_duplicate_records_service.rb
@@ -8,10 +8,35 @@ class CreateDuplicateRecordsService < ServiceBase
   end
 
   def perform 
-    open(@file_uri) do |f|
-      f.each_line do |line|
-        puts "#{line["*SupporterID*"]}"
-      end 
+
+    # must read the full file into memory, to group it into chunks based
+    # on dupe pair ids
+    file = SmarterCSV.process(open(URI.parse(@file_uri)),
+                       csv_import_options)
+
+    dupe_sets = file.group_by { |line| line["no."] }
+
+    dupe_sets.each do |set|
+      records = set.second #an array of the actual dupe record hashes
+      primary_record_id = records.first["*supporterid*"]
+      records.shift
+      duplicate_record_ids = records.map { |r| r["*supporterid*"] }
+      DuplicateRecord.create!(record_type: "supporter", 
+                              primary_record_id: primary_record_id,
+                              duplicate_record_ids: duplicate_record_ids)
     end
   end
+
+  private 
+
+    def csv_import_options
+      {
+          #chunk_size: chunk_size,
+          strip_whitespace: true,
+          strings_as_keys: true,
+          remove_empty_values: false
+          #remove_unmapped_keys: true,
+          #key_mapping: @import_mapping.mappings
+      }
+    end
 end
