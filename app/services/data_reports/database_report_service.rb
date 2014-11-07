a124f3710d6ab5a74e25944d93d18e8743b1263e
cdale77 <cdale77@gmail.com>
2014-11-06 22:20:01 -0800

    full database export

--- app/services/data_reports/database_report_service.rb
+++ app/services/data_reports/database_report_service.rb
@@ -1,9 +1,25 @@
 class DatabaseReportService < ServiceBase
 
   def perform
-    Rails.application.eager_load! unless Rails.env.production?
-    models = ActiveRecord::Base.descendants
+    p = Axlsx::Package.new
+    workbook = p.workbook
+
+    export_models.each do |model|
+      workbook.add_worksheet(name: "#{model.name.pluralize.downcase}") do |sheet|
+        sheet.add_row model.column_names
+        model.all.find_each do |record|
+          sheet.add_row record.attributes.values
+        end
+      end
+    end
+
+    return p.to_stream # returns a StringIO, good for paperclip
   end
 
-
+  private
+    def export_models
+      Rails.application.eager_load! unless Rails.env.production?
+      ActiveRecord::Base.descendants.select { |model| model.name.exclude?("::") }
+    end
 end
+
