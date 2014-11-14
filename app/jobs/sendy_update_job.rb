77c56a54228cc730e6f6dfa31584b5ae6e60bebb
cdale77 <cdale77@gmail.com>
2014-11-13 16:59:24 -0800

    turn off sendy updates

--- app/jobs/sendy_update_job.rb
+++ app/jobs/sendy_update_job.rb
@@ -4,7 +4,7 @@ class SendyUpdateJob < ActiveJob::Base
   queue_as :default
 
   def perform
-    updates = SendyUpdate.where(success: false)
-    Sendy::PerformUpdates.perform(updates)
+    #updates = SendyUpdate.where(success: false)
+    #Sendy::PerformUpdates.perform(updates)
   end
 end
