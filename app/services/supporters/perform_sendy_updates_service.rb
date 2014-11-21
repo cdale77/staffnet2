9a0fce5051fe36e21a0824de442a0f568d4231f3
cdale77 <cdale77@gmail.com>
2014-11-21 11:04:42 -0800

    tweak to sendy service

--- app/services/supporters/perform_sendy_updates_service.rb
+++ app/services/supporters/perform_sendy_updates_service.rb
@@ -2,9 +2,17 @@ class PerformSendyUpdatesService < ServiceBase
 
   def perform
     SendyUpdate.where(success: false).find_each(batch_size: 250) do |update|
-      puts "Performing update id #{update.id}"
-      klass = "Sendy#{update.action.classify}Job".constantize
-      klass.perform_later(update.id)
+      begin
+        puts "Performing update id #{update.id}"
+        klass = "Sendy#{update.action.classify}Job".constantize
+        klass.perform_later(update.id)
+        #horrible hack to avoid race conditions and keep things stable. When
+        #processing > 50 updates or so sometimes successful updates are not
+        #marked as successful. I think it's because Sendy does not return
+        #success, but still grabs the data, when it's hit too fast
+        sleep 5
+      rescue
+      end
     end
   end
 end
