208f35c9b23a5f5c07f7a5afb910c27a83ebe96b
cdale77 <cdale77@gmail.com>
2014-12-24 16:50:05 -0800

    update aws config

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -2,6 +2,6 @@ S3DirectUpload.config do |c|
   c.access_key_id     = ENV["AWS_ACCESS_KEY_ID"]
   c.secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
   c.bucket            = ENV["UPLOAD_BUCKET"]
-  c.region            = "s3-us-east-1"
+  c.region            = "s3-us-west-1"
   c.url               = "https://#{c.bucket}.s3.amazonaws.com/"
 end
