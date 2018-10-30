1f229bf4cc3deb06199ef2f30551e44205cf9e4c
Anthony Sanchez <anthony.sanchez.sf@gmail.com>
2018-10-30 15:00:36 -0700

    Explicit parameters for shift

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -71,7 +71,7 @@ class DonationsController < ApplicationController
   def edit
     @donation = Donation.find(params[:id])
     @supporter = @donation.supporter
-    @shifts = Shift.last_three_weeks
+    @shifts = Shift.where('created_at > ?', 3.weeks.ago)
     authorize @donation
   end
 
